00eca4a6b757c00c5f0617760134a5a61b89dae4
afwe <q936017948@126.com>
2021-12-04 15:09:18 +0800

    haha sb

--- server.js
+++ server.js
@@ -15,23 +15,24 @@ let sslOptions={
 var server=http.createServer(app);
 const https_server = require('https').createServer(sslOptions, app);
 https_server.listen(443,'0.0.0.0');
-server.listen(80,'0.0.0.0');
+server.listen(8880,'0.0.0.0');
 let io = socketio(https_server);
 var masterId = null;
 var slaveId;
+var masterMap = new Map();
 app.get('/getRooms',function  (req,res,next) {
     let roomObject = io.sockets.adapter.rooms;
     let response_data = [];
-    console.log(roomObject);
     for(let name in roomObject){
+        console.log(roomObject);
         if(roomObject[name].sockets[name]==undefined) response_data.push(
                 {
-                    roomName : name,
+                    id : name,
+                    name : `第${name}号房间`,
                     roomCandidates : roomObject[name].length
                 }
             );
     }
-    console.log(response_data);
     res.type('application/json');
     res.jsonp(response_data);
 });
@@ -47,7 +48,7 @@ io.sockets.on('connection',(socket)=>{
         //socket.to(room).emit('message',room,message);
     });
     socket.on('join',(room)=>{
-        console.log(socket.id);
+        console.log('join'+socket.id);
         socket.join(room);
         let theRoom = io.sockets.adapter.rooms[room];
         let users = (theRoom)? Object.keys(theRoom.sockets).length : 0;
